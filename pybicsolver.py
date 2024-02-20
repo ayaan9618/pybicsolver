ecc8ae68386ba475b204289b6a20cdd0e7ac670c
Mohammed Ayaan ur Rahman <135800053+ayaan9618@users.noreply.github.com>
2024-02-20 11:24:36 +0530

    Add files via upload

---
+++ pybicsolver.py
@@ -0,0 +1,30 @@
+# Simple calculator pybicsolver made by ayaan
+
+print("Welcome to simple calculator pybicsolver")
+print("This is basic calculator")
+ 
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+
+
+if choice == '1':
+    print(num1, "+", num2, "=", num1 + num2)
+elif choice == '2':
+    print(num1, "-", num2, "=", num1 - num2)
+elif choice == '3':
+    print(num1, "*", num2, "=", num1 * num2)
+elif choice == '4':
+    print(num1, "/", num2, "=", num1 / num2)
+else:
+    print("Invalid input")
+print("thanks for using pybicsolver calculator .To use again, Run again")
