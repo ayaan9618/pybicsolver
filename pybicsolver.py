6f969428cf559a434ae21632f46d87e89e9d7667
Mohammed Ayaan ur Rahman <135800053+ayaan9618@users.noreply.github.com>
2024-08-24 23:44:25 +0530

    Update pybicsolver.py

--- pybicsolver.py
+++ pybicsolver.py
@@ -25,7 +25,8 @@ elif choice == '2':
 elif choice == '3':
     print(num1, "*", num2, "=", num1 * num2)
 elif choice == '4':
- 
+    if num2==0:
+        print("Error! Division by zero is not allowed")
     print(num1, "/", num2, "=", num1 / num2)
 else:
     print("Invalid input")
